d4ae357df92538e0dfb6333be84f10df609e7aba
BilelHimeur <bilelhimeur@gmail.com>
2018-07-10 12:48:06 +0200

    11 adding getPriceByWeight method implementation

--- src/main/java/com/zsoft/supermarketpricing/utils/UnitConvertor.java
+++ src/main/java/com/zsoft/supermarketpricing/utils/UnitConvertor.java
@@ -1,5 +1,6 @@
 package com.zsoft.supermarketpricing.utils;
 
+import com.zsoft.supermarketpricing.exceptions.ConversionException;
 import com.zsoft.supermarketpricing.models.enums.Unit;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +12,28 @@ public class UnitConvertor implements BiFunction<Unit, Unit, Float> {
 
     @Override
     public Float apply(Unit inputUnit, Unit outputUnit) {
-        return 0F;
+        if (inputUnit == null) {
+            throw new NullPointerException("input unit is null");
+        }
+        if (outputUnit == null) {
+            throw new NullPointerException("output unit is null.");
+        }
+        if (outputUnit == inputUnit) {
+            return 1F;
+        }
+
+        switch (inputUnit) {
+            case OUNCE:
+                return 1 / 16F;
+            case POUND:
+                return 16F;
+            default:
+                try {
+                    throw new ConversionException();
+                } catch (ConversionException e) {
+                    e.printStackTrace();
+                }
+        }
+        return 1F;
     }
 }
