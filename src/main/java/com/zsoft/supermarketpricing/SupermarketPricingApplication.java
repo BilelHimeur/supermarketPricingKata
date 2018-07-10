4f66f5f4efc1e8b0651b1a16a5808e9560d95d51
BilelHimeur <bilelhimeur@gmail.com>
2018-07-10 11:00:31 +0200

    1 Initialize spring boot application

---
+++ src/main/java/com/zsoft/supermarketpricing/SupermarketPricingApplication.java
@@ -0,0 +1,12 @@
+package com.zsoft.supermarketpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketPricingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketPricingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zsoft/supermarketpricing/SupermarketPricingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zsoft.supermarketpricing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupermarketPricingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
