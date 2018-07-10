8f6c5efd5cd03187ca3ce066627b66e6faa90116
BilelHimeur <bilelhimeur@gmail.com>
2018-07-10 12:28:33 +0200

    8 adding getSimplePrice method implementation

--- src/main/java/com/zsoft/supermarketpricing/services/PricingService.java
+++ src/main/java/com/zsoft/supermarketpricing/services/PricingService.java
@@ -1,5 +1,6 @@
 package com.zsoft.supermarketpricing.services;
 
+import com.zsoft.supermarketpricing.exceptions.ProductNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,16 @@ public class PricingService {
 
     private ProductService productService;
 
+
     @Autowired
     public PricingService(ProductService productService) {
         this.productService = productService;
     }
 
-    public double getSimplePrice(long productId, int quantity) {
-        return 0;
+    public double getSimplePrice(long productId, int quantity) throws ProductNotFoundException {
+        return productService.getProductById(productId)
+                .map(p -> p.getPrice().getValue() * quantity)
+                .orElseThrow(() -> new ProductNotFoundException(productId));
     }
+
 }
